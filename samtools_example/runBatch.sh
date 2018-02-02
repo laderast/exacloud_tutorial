cfa9eb1601be8d1f19f573f51e827658b0234b5a
laderast <laderast@ohsu.edu>
2018-02-01 21:13:06 -0800

    added batch script1

--- samtools_example/runBatch.sh
+++ samtools_example/runBatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1 #request 1 node
 #SBATCH --array=1-6
-#SBATCH --tasks-per-node=6 ##we want our node to do 2 tasks concurrently
+#SBATCH --tasks-per-node=3 ##we want our node to do 2 tasks concurrently
 #SBATCH --cpus-per-task=1 ##ask for  CPUS (3 * 1)
 #SBATCH --mem-per-cpu=1gb ## request 1 gigabyte per cpu
 #SBATCH --time=00:05:00 ## ask for 5 minutes on the node
